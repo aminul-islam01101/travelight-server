7aa70b67081880c59e5636bc4b82cd309623dfea
aminulislam01101 <aminul.islam01101@gmail.com>
2022-10-30 03:41:29 +0600

    mongo introduced

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,60 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-empty */
 /* eslint-disable no-underscore-dangle */
 const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
 const cors = require("cors");
 const categories = require("./data/categories.json");
 const hotels = require("./data/travelight.json");
+// mongo credentials
+// Username: adminTravelight
+// Password: 8HfJ014ijrVFXa9N
+const uri = 'mongodb+srv://adminTravelight:8HfJ014ijrVFXa9N@cluster0.5ty8ljz.mongodb.net/?retryWrites=true&w=majority';
+const client = new mongodb_1.MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: mongodb_1.ServerApiVersion.v1,
+});
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const userCollection = client.db('adminTravelight').collection('users');
+        const users = { name: 'some2', email: 'some2@gmail.com' };
+        const result = yield userCollection.insertOne(users);
+        console.log(result);
+    }
+    finally {
+    }
+});
+run().catch((err) => console.log(err));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
 app.use(cors());
+const users = [
+    { id: 1, name: 'some1', email: 'some1@gmail.com' },
+    { id: 2, name: 'some2', email: 'some2@gmail.com' },
+    { id: 3, name: 'some3', email: 'some3@gmail.com' },
+];
 app.get('/', (_req, res) => {
     res.send('Express + TypeScript Server');
 });
 app.get('/hotel', (req, res) => {
     res.send(categories);
+});
+app.get('/users', (req, res) => {
+    res.send(users);
 });
 app.get('/hotels/:id', (req, res) => {
     const { id } = req.params;
--- index.ts
+++ index.ts
@@ -1,19 +1,48 @@
+/* eslint-disable no-empty */
 /* eslint-disable no-underscore-dangle */
 import express, { Express, Request, Response } from 'express';
+import { MongoClient, MongoClientOptions, ServerApiVersion } from 'mongodb';
 
 import cors = require('cors');
 import categories = require('./data/categories.json');
 import hotels = require('./data/travelight.json');
 
+// mongo credentials
+// Username: adminTravelight
+// Password: 8HfJ014ijrVFXa9N
+
+const uri =
+    'mongodb+srv://adminTravelight:8HfJ014ijrVFXa9N@cluster0.5ty8ljz.mongodb.net/?retryWrites=true&w=majority';
+const client: MongoClient = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+} as MongoClientOptions);
+
+interface Users {
+    name: string;
+    email: string;
+}
+const run = async () => {
+    try {
+        const userCollection = client.db('adminTravelight').collection<Users>('users');
+        const users = { name: 'some2', email: 'some2@gmail.com' };
+
+        const result = await userCollection.insertOne(users);
+        console.log(result);
+    } finally {
+    }
+};
+run().catch((err) => console.log(err));
+
 const app: Express = express();
 const port = process.env.PORT || 8000;
 app.use(cors());
 const users = [
-    {id:1, name: 'some1', email: "some1@gmail.com"},
-    {id:2, name: 'some2', email: "some2@gmail.com"},
-    {id:3, name: 'some3', email: "some3@gmail.com"},
+    { id: 1, name: 'some1', email: 'some1@gmail.com' },
+    { id: 2, name: 'some2', email: 'some2@gmail.com' },
+    { id: 3, name: 'some3', email: 'some3@gmail.com' },
 ];
-
 
 app.get('/', (_req, res: Response) => {
     res.send('Express + TypeScript Server');
